6b2b16f9854674f5211bc54009636e0dc09d41b8
Philipp <philipp.lackner@yahoo.de>
2020-04-12 20:11:05 +0200

    retrofit setup

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/Article.kt
@@ -0,0 +1,15 @@
+package com.androiddevs.mvvmnewsapp
+
+
+import com.google.gson.annotations.SerializedName
+
+data class Article(
+    val author: String,
+    val content: String,
+    val description: String,
+    val publishedAt: String,
+    val source: Source,
+    val title: String,
+    val url: String,
+    val urlToImage: String
+)
---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "6953a7421bb749c589e687746c85eee8"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
