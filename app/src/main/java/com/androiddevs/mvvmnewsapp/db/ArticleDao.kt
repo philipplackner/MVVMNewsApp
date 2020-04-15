42a6b0ac7ad407dfa7bcbb6962334d92229cafbb
Philipp <philipp.lackner@yahoo.de>
2020-04-15 15:11:18 +0200

    fixed some issues

--- app/src/main/java/com/androiddevs/mvvmnewsapp/db/ArticleDao.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/db/ArticleDao.kt
@@ -1,5 +1,6 @@
 package com.androiddevs.mvvmnewsapp.db
 
+import androidx.lifecycle.LiveData
 import androidx.room.*
 import com.androiddevs.mvvmnewsapp.models.Article
 
@@ -10,7 +11,7 @@ interface ArticleDao {
     suspend fun upsert(article: Article): Long
 
     @Query("SELECT * FROM articles")
-    suspend fun getAllArticles(): List<Article>
+    fun getAllArticles(): LiveData<List<Article>>
 
     @Delete
     suspend fun deleteArticle(article: Article)
