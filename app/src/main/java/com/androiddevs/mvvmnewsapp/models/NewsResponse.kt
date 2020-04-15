089f8671870da4020b44515fd667744d5220d727
Philipp <philipp.lackner@yahoo.de>
2020-04-15 14:48:30 +0200

    fixed some issues

--- app/src/main/java/com/androiddevs/mvvmnewsapp/models/NewsResponse.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/models/NewsResponse.kt
@@ -1,8 +1,6 @@
 package com.androiddevs.mvvmnewsapp.models
 
 
-import com.androiddevs.mvvmnewsapp.models.Article
-
 data class NewsResponse(
     val articles: List<Article>,
     val status: String,
