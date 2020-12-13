69b28cc5ecd3edaede1c1b8564a0d835272d5494
Alif Taufik <alif.tr.aminuddin@gmail.com>
2020-12-13 23:37:21 +0700

    Update Gradle and dependencies version
    Implement Fragment Navigation
    
    Implement Bottom Navigation Bar

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/fragments/ArticleFragment.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.androiddevs.mvvmnewsapp.R
+
+class ArticleFragment : Fragment(R.layout.fragment_article) {
+
+}
---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/fragments/SearchNewsFragment.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.androiddevs.mvvmnewsapp.R
+
+class SearchNewsFragment : Fragment(R.layout.fragment_search_news) {
+
+}
