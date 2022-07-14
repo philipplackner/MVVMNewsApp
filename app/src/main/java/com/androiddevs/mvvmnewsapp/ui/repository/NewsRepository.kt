9cf548f2d8b8e349e0be1db8bf56c09b402dcf53
KDetnoi <105243557+KDetnoi@users.noreply.github.com>
2022-07-14 15:48:27 +0200

    working v1

--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/repository/NewsRepository.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/repository/NewsRepository.kt
@@ -1,9 +1,11 @@
 package com.androiddevs.mvvmnewsapp.ui.repository
 
+import com.androiddevs.mvvmnewsapp.ui.api.RetrofitInstance
 import com.androiddevs.mvvmnewsapp.ui.db.ArticleDatabase
 
 class NewsRepository(
     val db: ArticleDatabase
 ) {
-
+    suspend fun getBreakingNews(countryCode: String, pageNumber: Int) =
+        RetrofitInstance.api.getBreakingNews(countryCode, pageNumber)
 }
--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/ui/NewsViewModel.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/ui/NewsViewModel.kt
@@ -1,10 +1,41 @@
 package com.androiddevs.mvvmnewsapp.ui.ui
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.androiddevs.mvvmnewsapp.ui.models.NewsResponse
 import com.androiddevs.mvvmnewsapp.ui.repository.NewsRepository
+import com.androiddevs.mvvmnewsapp.ui.util.Resource
+import kotlinx.coroutines.launch
+import retrofit2.Response
 
 class NewsViewModel(
     val newsRepository: NewsRepository
 ) : ViewModel(){
+    //The fragment subscribe to the livedata and will know when things gets updated
+    val breakingNews: MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
+    var breakingNewsPage = 1
 
+    init {
+        getBreakingNews("no")
+    }
+
+    //Used only if this viewModel is alive
+    fun getBreakingNews(countryCode: String) = viewModelScope.launch {
+        breakingNews.postValue(Resource.Loading())
+        val response = newsRepository.getBreakingNews(countryCode, breakingNewsPage)
+        breakingNews.postValue(handleBreakingNewsResponse(response))
+    }
+
+
+    private fun handleBreakingNewsResponse(response: Response<NewsResponse>) : Resource<NewsResponse>{
+        //If Success return response
+        if (response.isSuccessful){
+            response.body()?.let { resultResponse ->
+                return Resource.Success(resultResponse)
+            }
+        }
+        //Else return Error message
+        return Resource.Error(response.message())
+    }
 }
