bf2e205fc26d2c302aa03f30903440251f140fe9
KDetnoi <105243557+KDetnoi@users.noreply.github.com>
2022-07-14 14:05:27 +0200

    more DAO

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/db/ArticleDAO.kt
@@ -0,0 +1,22 @@
+package com.androiddevs.mvvmnewsapp.ui.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.androiddevs.mvvmnewsapp.ui.Article
+
+
+@Dao
+interface ArticleDAO {
+
+    //Update or Insert Article in articles table
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun upsert(article: Article): Long
+
+    //Search for all Article
+    @Query("SELECT * FROM articles")
+    fun getAllArticles(): LiveData<List<Article>>
+
+    //Delete Article
+    @Delete
+    suspend fun deleteArticle(article: Article)
+}
