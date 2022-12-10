ecd9cab92f2b7008e1f5d94fa45f42da97a39c1e
Burak Demir <burakdemirafl@gmail.com>
2022-12-10 19:28:49 +0300

    DAO and Article Setup Room

--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/Article.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/Article.kt
@@ -1,6 +1,15 @@
 package com.androiddevs.mvvmnewsapp.ui
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(
+    tableName = "articles"
+)
 data class Article(
+    @PrimaryKey(autoGenerate = true)
+    var id: Int? = null,
     val author: String,
     val content: String,
     val description: String,
---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/ArticleDao.kt
@@ -0,0 +1,20 @@
+package com.androiddevs.mvvmnewsapp.ui
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface ArticleDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun upsert(article: Article):Long
+
+    @Query("SELECT * FROM articles")
+    fun getAllArticles():LiveData<List<Article>>
+
+    @Delete
+    suspend fun deleteArticle(article: Article)
+}
