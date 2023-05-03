d923c67fb3c1fd7ad1c25e17fbeaa5f0bf9f80a1
Aditya Mishra <adityamishra2217@gmail.com>
2023-05-03 22:10:32 +0530

    First commit to test the rebase

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/NewsResponse.kt
@@ -0,0 +1,12 @@
+package com.androiddevs.mvvmnewsapp
+
+
+import com.google.gson.annotations.SerializedName
+
+data class NewsResponse(
+    val articles: List<Article>,
+    val status: String,
+    val totalResults: Int
+)
+
+//Basic Update for just learning rebase.
